b7cd67862452f56104c79890ad1b9b33950b97a7
tgpretender <jtpretender@yahoo.com>
2021-10-23 10:30:12 -0500

    fixing client

--- db/client.js
+++ db/client.js
@@ -1,4 +1,8 @@
 const { Client } = require('pg'); 
-const client = new Client(process.env.DATABASE_URL || 'postgresql://pppadmin:123456@localhost:5432/graceshopper-dev');
+const client = new Client({
+	connectionString: process.env.DATABASE_URL || 'postgresql://pppadmin:123456@localhost:5432/graceshopper-dev',
+	ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  });
+
 
 module.exports =  client 
